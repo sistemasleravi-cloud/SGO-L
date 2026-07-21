93aae76bad2e2e2a6466deab384d499f5093d195
sistemasleravi-cloud <sistemasleravi@gmail.com>
2026-07-21 12:12:00 -0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  // Apuntamos directamente a tu servidor en producción
+  baseURL: 'https://leravi-backend-api.onrender.com/api/',
 });
 
 api.interceptors.request.use(config => {
